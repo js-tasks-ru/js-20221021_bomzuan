2c28af6ab48ea193714e7efd7f3237c9de879c5a
Yevgeniy Borisovsky <yev.bor@yandex.ru>
2022-10-31 22:42:53 +0200

    Getter function is done

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,9 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const stringToArray= path.split('.');
+  console.log(stringToArray);
+  return function(obj){
+    return stringToArray.reduce((prev, curr) =>prev?.[curr], obj)
+  }
 }
