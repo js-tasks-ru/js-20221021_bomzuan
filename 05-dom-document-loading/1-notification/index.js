f7cc8ad6da84d02a23e35e0650b691e50ec14377
Yevgeniy Borisovsky <yev.bor@yandex.ru>
2022-11-10 15:22:16 +0200

    one test faild

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,70 @@
 export default class NotificationMessage {
+  static resetHandler;
+  element;
+  timeoutID;
+  constructor(
+    message ='',
+    {
+      duration = 2000,
+      type = 'success'
+              }={}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
 
+    this.render();
+  }
+
+  get template(){
+    return `
+      <div id="foo" class="notification ${this.statusChecker}" style="--value:${this.duration/1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">Notification</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `
+  }
+
+  render(){
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.template;
+    this.element= wrapper.firstElementChild;
+  }
+
+  show(parent = document.body){
+    if(NotificationMessage.resetHandler){
+      NotificationMessage.resetHandler.removeDuplicate();
+    }
+    parent.append(this.element)
+    // this.timerRemoval();
+    this.timeoutID = setTimeout(()=>{
+      this.removeDuplicate()
+    }, this.duration)
+
+    NotificationMessage.resetHandler = this;
+  }
+
+  get statusChecker(){
+    return this.type === 'success' ? 'success': 'error';
+  }
+
+  timerRemoval(){
+  }
+
+  removeDuplicate(){
+      clearTimeout(this.timeoutID);
+
+      if(this.element){
+        this.element.remove()
+      }
+  }
+  destroy(){
+    this.removeDuplicate();
+    this.element = null;
+    NotificationMessage.resetHandler = null;
+  }
 }
